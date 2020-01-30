7133bf731f41367e16ead3cdac4dd5d9afb781f4
kelvin27315 <seven_units@yahoo.co.jp>
2020-01-31 07:33:35 +0900

    データの保存機能を追加

--- ponymofu_counter.py
+++ ponymofu_counter.py
@@ -9,6 +9,13 @@ import datetime as dt
 import pandas as pd
 import re
 
+def extract_content(text):
+        #HTMLタグ, URL, LSEP,RSEP, HTML特殊文字を取り除く
+        text = re.sub(r"<[^>]*?>", "", text)
+        text = re.sub(r"(https?|ftp)(:\/\/[-_\.!~*\'()a-zA-Z0-9;\/?:\@&=\+\$,%#]+)", "", text)
+        text = re.sub(r"[  ]", "", text)
+        return(text)
+
 class Ponytail_Counter(Mastodon):
     def __init__(self, id=22674):
         self.path = Path(__file__).parent.resolve()
@@ -38,18 +45,26 @@ class Ponytail_Counter(Mastodon):
 
     def count_ponytail(self):
         toots = self.get_toots()
-        #時間内のtootのみcontentを追加する
+
+        temp = []
+        t_append = temp.append
         for toot in toots:
             time = toot["created_at"].astimezone(timezone("Asia/Tokyo"))
             if self.day_start <= time and time < self.day_end:
-                #CWの呟きの場合,隠されている方も追加する
-                text = toot["content"]
-                text = "{} {}".format(toot["content"], toot["spoiler_text"]) if toot["sensitive"] == True else toot["content"]
+                t_append(toot)
+                text = "{} {}".format(toot["content"], toot["spoiler_text"])
                 #もふってるのを探して数える
                 if re.search(r"(ぽにて|ポニテ)(もふ|モフ)り(たい|てぇ)", text) is not None:
                     self.ponytail += 1
                 if re.search(r"毛玉(吐|は)いた", text) is not None:
                     self.kedama += 1
+
+        self.toots = pd.DataFrame({
+            "date":         [toot["created_at"].astimezone(timezone("Asia/Tokyo")) for toot in temp],
+            "content":      [extract_content(toot["content"]) for toot in temp],
+            "spoiler_text": [extract_content(toot["spoiler_text"]) for toot in temp],
+            "visibility":   [toot["visibility"] for toot in temp]
+        })
 
     def post(self):
         """
@@ -61,10 +76,19 @@ class Ponytail_Counter(Mastodon):
         )
         self.status_post(status=post, visibility="unlisted")
 
+    def data_save(self):
+        file_name = "{}.pkl".format(dt.date.today() - dt.timedelta(days=1))
+        self.toots.to_pickle(self.path/"data"/"all"/file_name)
+        pickup_list = [True if re.search(r"(ぽにて|ポニテ)(もふ|モフ)り(たい|てぇ)", "{} {}".format(toot["content"],toot["spoiler_text"])) is not None else False for i, toot in self.toots.iterrows()]
+        self.toots[pickup_list].to_pickle(self.path/"data"/"ponytail"/file_name)
+        pickup_list = [True if re.search(r"毛玉(吐|は)いた", "{} {}".format(toot["content"],toot["spoiler_text"])) is not None else False for i, toot in self.toots.iterrows()]
+        self.toots[pickup_list].to_pickle(self.path/"data"/"kedama"/file_name)
+
 def main():
     kasaki = Ponytail_Counter()
     kasaki.count_ponytail()
     kasaki.post()
+    kasaki.data_save()
 
 if __name__ == "__main__":
     main()
