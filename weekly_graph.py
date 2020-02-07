7904c097b9cb779e3b2f5e5bc7c74bdb17c4a240
kelvin27315 <seven_units@yahoo.co.jp>
2020-02-07 19:35:06 +0900

    折れ線グラフに変えた

--- weekly_graph.py
+++ weekly_graph.py
@@ -4,43 +4,51 @@
 
 from mastodon import Mastodon
 from pathlib import Path
+from matplotlib import ticker
 import matplotlib.pyplot as plt
 import datetime as dt
 import pandas as pd
+import seaborn as sns
+sns.set()
 import japanize_matplotlib
 
-class Yoroizuka(Mastodon):
+class Mofumofu_visualizer(Mastodon):
     def __init__(self, id=22674):
         self.id = id
         self.path = Path(__file__).parent.resolve()
         self.day = dt.date.today() - dt.timedelta(days=1)
 
-        super(Yoroizuka, self).__init__(
+        super(Mofumofu_visualizer, self).__init__(
             client_id = self.path/"token"/"clientcred.secret",
             access_token = self.path/"token"/"usercred.secret",
             api_base_url = "https://mstdn.poyo.me"
         )
 
     def make_graph(self):
-        x1 = range(7)
-        x2 = [i + 0.4 for i in range(7)]
-        file_paths = [str(self.path / "data" / "kedama" / "{}.pkl".format(self.day - dt.timedelta(days=6-i))) for i in range(7)]
+        days = [str(self.day - dt.timedelta(days=6-i))for i in range(7)]
+        file_paths = [str(self.path / "data" / "kedama" / "{}.pkl".format(day)) for day in days]
         kedama = [len(pd.read_pickle(file_path)) for file_path in file_paths]
-        file_paths = [str(self.path / "data" / "ponytail" / "{}.pkl".format(self.day - dt.timedelta(days=6-i))) for i in range(7)]
+        file_paths = [str(self.path / "data" / "ponytail" / "{}.pkl".format(day)) for day in days]
         ponytail = [len(pd.read_pickle(file_path)) for file_path in file_paths]
+        days = [day[-5:] for day in days]
+        data = pd.concat([pd.DataFrame({
+            "種類": "ぽにて",
+            "回数": ponytail,
+            "日付": days
+        }),pd.DataFrame({
+            "種類": "毛玉",
+            "回数": kedama,
+            "日付": days
+        })])
 
-        plt.figure(figsize=(8,5))
-        plt.bar(x1,kedama, 0.4,label="毛玉",color="#ed86b3")
-        plt.bar(x2,ponytail, 0.4,label="ぽにて",color="#6cc5ed")
+        fig = plt.figure()
+        sns.lineplot(x="日付", y="回数", hue="種類", style="種類", markers=True, dashes=False, data=data)
+        #color="#ed86b3", color="#6cc5ed"
         week = self.day - dt.timedelta(days=7)
-        plt.title("{}年{}月{}日〜{}年{}月{}日の間で、ぽにてをモフろうとした回数、毛玉を吐いた回数".format(
+        plt.title("{}年{}月{}日〜{}年{}月{}日の間で、\nぽにてをモフろうとした回数、毛玉を吐いた回数".format(
             week.year,week.month,week.day, self.day.year,self.day.month, self.day.day
         ))
-        plt.xlabel("日付")
-        plt.ylabel("回数")
-        days = [str(self.day - dt.timedelta(days=6-i))[-5:] for i in range(7)]
-        plt.xticks([i+0.2 for i in range(7)], days)
-        plt.legend()
+        plt.gca().get_yaxis().set_major_locator(ticker.MaxNLocator(integer=True))
         plt.savefig(self.path / "weekly_graph.png")
 
     def post(self):
@@ -53,7 +61,7 @@ class Yoroizuka(Mastodon):
         self.status_post(post, media_ids = media, visibility="unlisted")
 
 def main():
-    mizore = Yoroizuka()
+    mizore = Mofumofu_visualizer()
     mizore.make_graph()
     mizore.post()
 
